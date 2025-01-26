acdb43269e0e46701e66b7a67c56ce3bb95060c7
Sayma-Shimu <ahmedsayma399@gmail.com>
2025-01-26 12:34:22 +0600

    Switch case added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+/*
 let month = prompt();
 
 if(month==='January'){
@@ -52,6 +52,48 @@ else if(month==='December'){
 else{
     document.write('Invalid')
 
-}
+}   */
+
+
+
+    /*---------------switch--------------*/
+
+    let days = +prompt()
+
+    switch (days){
+
+        case 1:
+            document.write('Sunday');
+            break;
+
+        case 2:
+            document.write('Monday');
+            break;
+
+        case 3:
+            document.write('Tuesday');
+            break;
+
+        case 4:
+            document.write('Wednesday');
+            break;
+
+        case 5:
+            document.write('Thursday');
+            break;
+
+        case 6:
+            document.write('Friday');
+            break;
+
+        case 7:
+            document.write('Saturday');
+            break;
+
+            default :
+            document.write('Invalid Number');
+
+    } 
+
 
 
