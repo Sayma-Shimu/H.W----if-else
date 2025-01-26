3769181bfc1f85bfb2938b2eb2ac26619cd923d8
Sayma-Shimu <ahmedsayma399@gmail.com>
2025-01-26 12:06:42 +0600

    home work added

---
+++ index.js
@@ -0,0 +1,51 @@
+let month = prompt();
+
+if(month==='January'){
+    document.write(1);
+}
+
+else if(month==='February'){
+    document.write(2);
+}
+
+else if(month==='March'){
+    document.write(3);
+}
+
+else if(month==='April'){
+    document.write(4);
+}
+
+else if(month==='May'){
+    document.write(5);
+}
+
+else if(month==='June'){
+    document.write(6);
+}
+
+else if(month==='July'){
+    document.write(7);
+}
+
+else if(month==='August'){
+    document.write(8);
+}
+
+else if(month==='September'){
+    document.write(9);
+}
+
+else if(month==='October'){
+    document.write(10);
+}
+
+else if(month==='November'){
+    document.write(11);
+}
+
+else if(month==='December'){
+    document.write(12);
+}
+
+
